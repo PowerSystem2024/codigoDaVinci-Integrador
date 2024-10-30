ec8ed46d8a91f2434a6591192af3f4c094244135
Santiago Torres <santitorresferrari@gmail.com>
2024-10-30 18:22:02 -0300

    Update Usuario.java
    agrego modificaciones a Usuario.java

--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/comunusuarios/modelo/Usuario.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/comunusuarios/modelo/Usuario.java
@@ -3,14 +3,17 @@ package com.davincicode.bibliotecaplus.comunusuarios.modelo;
 public class Usuario {
     private String rol;
     private String nombre;
+    private String apellido;
     private String correo;
 
     public Usuario() {
     }
 
     public Usuario(String rol, Long id , String nombre, String correo){
+    public Usuario(String rol, Long nombre, String apellido, String correo){
         this.rol = rol;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.correo = correo;
     }
     public Usuario(String nombre){
@@ -41,4 +44,11 @@ public class Usuario {
     public void setCorreo(String correo) {
         this.correo = correo;
     }
+    
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
 }
