e5e2e263c8c5a68adeef82aa5b0fb2b1f9a0c9a1
Santiago Torres <santitorresferrari@gmail.com>
2024-10-30 18:43:50 -0300

    Update Socio.java
    agrego modificaciones

--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionusuarios/modelo/Socio.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionusuarios/modelo/Socio.java
@@ -4,7 +4,10 @@ package com.davincicode.bibliotecaplus.gestionusuarios.modelo;
 import com.davincicode.bibliotecaplus.comunusuarios.modelo.Usuario;
 
 public class Socio extends Usuario {
-    /**Escribir atributos y comportamientos que necesitamos*/
+    private Long idSocio;
+    private String telefono;
+    private String nombreUsuarioLogin;
+    private String contraseniaLogin;
 
     private static int id = 1; /*generamos un atributo de clase y no de objeto que funciona como contador cada vez que se instancia un objeto*/
     private int idSocio; /*atributo id que va a tener cada objeto creado*/
@@ -12,4 +15,10 @@ public class Socio extends Usuario {
     public Socio() {
         idSocio = Socio.id++; /*el id del socio le ponemos el valor del contador*/
     }
+
+    public Socio(String rol, String nombre, String apellido, String correo, String telefono, String nombreUsuarioRegistro, String contraseniaUsuarioRegistro) {
+        super(rol, nombre, apellido, correo);
+        this.telefono = telefono;
+        this.nombreUsuarioLogin = nombreUsuarioRegistro;
+        this.contraseniaUsuarioLogin = contraseniaUsuarioRegistro
 }
