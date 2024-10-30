1ced6f84c11adaa4f12d43417b759707457c5ed4
Santiago Torres <santitorresferrari@gmail.com>
2024-10-30 18:27:04 -0300

    Update SesionSocioRepository.java
    Agrego modificaciones

--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionsesionsocio/repositorio/SesionSocioRepository.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionsesionsocio/repositorio/SesionSocioRepository.java
@@ -1,4 +1,7 @@
 package com.davincicode.bibliotecaplus.gestionsesionsocio.repositorio;
 
 public interface SesionSocioRepository {
+
+  void aniadirRegistroSocio(String nombre, String apellido, String correo, String telefono, String nombreUsuarioRegistro; String contraseniaUsuarioRegistro);
+  boolean permitirLoginSocion(String nombreUsuarioLoginIngresado, String contraseniaLoginIngresada);
 }
