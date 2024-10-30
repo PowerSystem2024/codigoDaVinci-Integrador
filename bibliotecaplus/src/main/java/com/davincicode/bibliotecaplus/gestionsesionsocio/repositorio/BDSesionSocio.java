45937854cb2cdd36d694b65d19bbcbc46e408d45
Santiago Torres <santitorresferrari@gmail.com>
2024-10-30 18:33:28 -0300

    Update BDSesionSocio.java
    agrego modificaciones

--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionsesionsocio/repositorio/BDSesionSocio.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionsesionsocio/repositorio/BDSesionSocio.java
@@ -12,5 +12,17 @@ public class BDSesionSocio implements SesionSocioRepository{
         this.registroSocios = new ArrayList<>();
     }
 
+@Override
+public boolean permitirLoginSocio(String nombreUsuarioLoginIngresado, String contraseniaLoginIngresada) {
+    if(!nombreUsuarioLoginIngresado.trim().isEmpty() && !contraseniaLoginIngresada.trim().isEmpty() ){
+        for(int i = 0; i < registroSocios.size(); i++){ 
+            //valido que nombre suario y contraseña coincidan con algún elemento de la lista
+            if(nombreUsuarioLoginIngresado.equalsIgnoreCase(registroSocios.get(i).getNombreUsuarioLogin())
+                    && contraseniaLoginIngresada.equalsIgnoreCase(registroSocios.get(i).getContraseniaLogin())) {
+                return true;
+            }
+        }
+    }
+    return false; /*por defecto no va a permitir el login*/
 
 }
