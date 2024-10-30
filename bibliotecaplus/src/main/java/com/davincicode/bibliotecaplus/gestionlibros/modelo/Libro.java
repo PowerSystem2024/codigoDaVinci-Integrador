f8253744b380004925a071a82d63ca3a19faf63e
Jonathan Suarez <jony35622@gmail.com>
2024-10-30 10:07:59 -0300

    Agrego id en Libro y en Socio para trabajar el autoincremento de los libros y de los socios para su atributo id

--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionlibros/modelo/Libro.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionlibros/modelo/Libro.java
@@ -1,5 +1,11 @@
 package com.davincicode.bibliotecaplus.gestionlibros.modelo;
 
 public class Libro {
+
+    private static int idLibro = 0; /*generamos un atributo de la clase y no de los objetos / idSocio lo incrementamos en 1 cada vez que se crea un objeto de tipo Socio*/
+
+    public Libro(){
+        idLibro = Libro.idLibro++; //para acceder al idLibro (atributo de clase) primero llamamos a la clase y luego al atributo, acá incrementamos en 1 en el constructor de la clase para aumentar en 1 el valor de idLibro cuando se instancia la clase
+    }
    /**Escribir atributos y comportamientos que necesitamos*/
 }
--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionusuarios/modelo/Socio.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionusuarios/modelo/Socio.java
@@ -5,6 +5,9 @@ import com.davincicode.bibliotecaplus.comunusuarios.modelo.Usuario;
 
 public class Socio extends Usuario {
 
+    private static int idSocio = 0;/*generamos un atributo de la clase y no de los objetos / idSocio lo incrementamos en 1 cada vez que se crea un objeto de tipo Socio*/
+
     public Socio() {
+        idSocio = Socio.idSocio++; //para acceder al idSocio (atributo de clase) primero llamamos a la clase y luego al atributo, acá incrementamos en 1 en el constructor de la clase para aumentar en 1 el valor de idSocio cuando se instancia la clase
     }
 }
