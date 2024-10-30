81079fc1bae92b4d4d7980eb7f837f5e56764385
HebeFacello <111471439+HebeFacello@users.noreply.github.com>
2024-10-30 10:48:58 -0300

    cambios en SesionSocioRepository.java

--- bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionsesionsocio/repositorio/SesionSocioRepository.java
+++ bibliotecaplus/src/main/java/com/davincicode/bibliotecaplus/gestionsesionsocio/repositorio/SesionSocioRepository.java
@@ -1,4 +1,6 @@
 package com.davincicode.bibliotecaplus.gestionsesionsocio.repositorio;
 
 public interface SesionSocioRepository {
+  void aniadirRegistroSocio (String nombre, String apellido, String correo, String telefono, String nombreUsuarioRegistro, String contraseniaUsuarioRegistro);
+  boolean permitirLoginSocio (String nombreUsuarioLoginIngresado, String contraseniaLoginIngresado);
 }
